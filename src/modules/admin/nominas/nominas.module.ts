9a26ba4802efffb26c89bc185e7007c18ee9013b
unknown <ramon.anchondo@gmail.com>
2026-09-01 18:05:58 -0600

    fix: importa explicitamente el AwsModule en el módulo

--- src/modules/admin/nominas/nominas.module.ts
+++ src/modules/admin/nominas/nominas.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NominasController } from './nominas.controller';
 import { NominasService } from './nominas.service';
 import { ReciboNomina } from './entities/recibo-nomina.entity';
+import { AwsModule } from '../../../shared/aws/aws.module'; // 🟢 Asegura importar el módulo compartido de S3
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ReciboNomina]),
+    AwsModule,   // 🟢 Agregamos el módulo aquí para proveer explícitamente el S3Service
   ],
   controllers: [NominasController],
   providers: [NominasService],
