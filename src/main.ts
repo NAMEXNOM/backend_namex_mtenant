7cc8d06fe4a5753b4f8929cef0c0fbbedf9ff56a
unknown <ramon.anchondo@gmail.com>
2026-09-05 18:31:02 -0600

    fix: firma de objeto en controlador lista para compilar en ec2

--- src/main.ts
+++ src/main.ts
@@ -54,7 +54,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.listen(process.env.PORT ?? 5002);
 }
 bootstrap();
 
