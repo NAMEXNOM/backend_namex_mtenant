ebaaab2f2428668117bf93d6ced1bd60bf9fca6a
unknown <ramon.anchondo@gmail.com>
2026-09-22 14:17:49 -0600

    debug: inyecta consola de auditoría dentro de roles

--- src/shared/guards/roles.guard.ts
+++ src/shared/guards/roles.guard.ts
@@ -1,3 +1,53 @@
+
+// src/shared/guards/roles.guard.ts
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException, Logger } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  private readonly logger = new Logger('RolesGuard-Auditoria');
+
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const handler = context.getHandler();
+    const rolesPermitidos = this.reflector.get<string[]>('roles', handler);
+    
+    const request = context.switchToHttp().getRequest();
+    const usuario = request.user;
+
+    // 🕵️‍♂️ LÍNEAS ESPÍA: Esto se imprimirá en los logs de PM2 al presionar el botón de carga
+    this.logger.log(`==================================================`);
+    this.logger.log(`🤖 ENDPOINT DETECTADO: ${handler.name}`);
+    this.logger.log(`🔑 ROLES EXIGIDOS EN EL DECORADOR: ${JSON.stringify(rolesPermitidos)}`);
+    this.logger.log(`👤 USUARIO DETECTADO EN REQUEST: ${JSON.stringify(usuario)}`);
+    this.logger.log(`==================================================`);
+
+    if (!rolesPermitidos) {
+      return true;
+    }
+
+    if (!usuario || !usuario.role) {
+      throw new ForbiddenException('No tienes un rol válido asignado en tu sesión.');
+    }
+
+    const rolUsuarioSuperior = usuario.role.trim().toUpperCase();
+    
+    const tienePermiso = rolesPermitidos
+      .map(role => role.trim().toUpperCase())
+      .includes(rolUsuarioSuperior);
+
+    if (!tienePermiso) {
+      throw new ForbiddenException('Acceso denegado: Este módulo es exclusivo para Administradores.');
+    }
+
+    return true;
+  }
+}
+
+
+/*
+
 // src/shared/guards/roles.guard.ts
 import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
@@ -36,3 +86,4 @@ export class RolesGuard implements CanActivate {
     return true;
   }
 }
+*/
